892773639daa02acd5a948e3f47e4b40ff09ab3b
Fay Vera <fvera.cavalcante@gmail.com>
2021-01-24 22:37:42 -0500

    commit tree db717ab01b76d925a7739323e9ef3106fd769cbd

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,16 +1,32 @@
+require 'pry'
 def starts_with_a_vowel?(word)
+if word.scan(/\[aeiouAEIOU]/)
+    true
+else
+    false
+end
 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+    text.scan(/\w+ing/)
 
 end
 
 def words_five_letters_long(text)
 
+    array = text.split(" ")
+    array.grep(/\b\w{5}\b/)
+
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
+    fist_letter = text.scan(/\w+/)[0
+    if (text.scan(/\./)) && (first_letter == first_letter.upcase)
+        true
+    else
+        false
+    end
 
 end
 
